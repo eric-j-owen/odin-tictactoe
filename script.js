5b989be7b0faf3a0220805b6102471b06a4a0c7a
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-02-15 11:52:19 -0500

    Adds player logic and win logic in GameController

--- script.js
+++ script.js
@@ -4,37 +4,88 @@ const GameBoard = (function () {
         [null, null, null],
         [null, null, null],
     ];
-    const getBoard = () => {
-        return board;
-    };
+    const getBoard = () => board;
     return { getBoard };
 })();
 
 function gameController() {
-
     const board = GameBoard.getBoard();
-    
+
     const player = [
         {
             name: "Player One",
-            token: "X",
+            move: "X",
         },
         {
             name: "Player Two",
-            token: "O",
+            move: "O",
         },
     ];
 
-    const isValidMove = (x, y) => {
-        return board[x][y] === null;
+    let currentPlayer = Math.round(Math.random()) === 0 ? player[0] : player[1];
+
+    const getCurrentPlayer = () => currentPlayer;
+    const switchCurrentPlayer = () =>
+        (currentPlayer = currentPlayer === player[0] ? player[1] : player[0]);
+    const isValidMove = (x, y) => board[x][y] === null;
+    const updateBoard = (x, y) => (board[x][y] = currentPlayer.move);
+    const checkWin = (board, player) => {
+        const consecutiveMovesForWin = 3;
+        let consecutiveMovesCount = 0;
+
+        // check rows
+        for (let row = 0; row < board.length; row++) {
+            consecutiveMovesCount = 0;
+            for (let col = 0; col < board[row].length; col++) {
+                if (board[row][col] === player.move) consecutiveMovesCount++;
+            }
+            if (consecutiveMovesCount === consecutiveMovesForWin)
+                return `${player.name} wins!`;
+        }
+        consecutiveMovesCount = 0;
+
+        // check cols
+        for (let col = 0; col < board.length; col++) {
+            consecutiveMovesCount = 0;
+            for (let row = 0; row < board[col].length; row++) {
+                if (board[row][col] === player.move) consecutiveMovesCount++;
+            }
+            if (consecutiveMovesCount === consecutiveMovesForWin)
+                return `${player.name} wins!`;
+        }
+        consecutiveMovesCount = 0;
+
+        // check diag
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][i] === player.move) consecutiveMovesCount++;
+        }
+        if (consecutiveMovesCount === consecutiveMovesForWin) {
+            return `${player.name} wins!`;
+        }
+        consecutiveMovesCount = 0;
+
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][board.length - 1 - i] === player.move)
+                consecutiveMovesCount++;
+        }
+        if (consecutiveMovesCount === consecutiveMovesForWin) {
+            return `${player.name} wins!`;
+        }
+        consecutiveMovesCount = 0;
+
+        return false;
     };
 
-    const updateBoard = (x, y) => {
-        board[x][y] = player[0].token;
+    return {
+        getCurrentPlayer,
+        switchCurrentPlayer,
+        isValidMove,
+        updateBoard,
+        checkWin,
     };
-
-    return { isValidMove, updateBoard };
 }
+
 const game = gameController();
-game.updateBoard(0,0)
-console.log(GameBoard.getBoard());
+while (!game.checkWin(GameBoard.getBoard(), game.getCurrentPlayer())) {
+    
+}
