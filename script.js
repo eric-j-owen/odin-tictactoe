15f0a450f7b518541a69b4f78662ef1cdb78c6d0
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-02-15 12:50:27 -0500

    adds gameloop for console and a draw condition

--- script.js
+++ script.js
@@ -24,6 +24,11 @@ function gameController() {
 
     let currentPlayer = Math.round(Math.random()) === 0 ? player[0] : player[1];
 
+    const printBoard = (board) => {
+        console.log(board[0]);
+        console.log(board[1]);
+        console.log(board[2]);
+    };
     const getCurrentPlayer = () => currentPlayer;
     const switchCurrentPlayer = () =>
         (currentPlayer = currentPlayer === player[0] ? player[1] : player[0]);
@@ -32,6 +37,23 @@ function gameController() {
     const checkWin = (board, player) => {
         const consecutiveMovesForWin = 3;
         let consecutiveMovesCount = 0;
+        let isDraw = true;
+
+        // check draw
+        for (let row = 0; row < board.length; row++) {
+            for (let col = 0; col < board[row].length; col++) {
+                if (board[row][col] === null) {
+                    isDraw = false;
+                    break;
+                }
+            }
+            if (!isDraw) {
+                break;
+            }
+        }
+        if (isDraw) {
+            return "It's a draw.";
+        }
 
         // check rows
         for (let row = 0; row < board.length; row++) {
@@ -39,8 +61,9 @@ function gameController() {
             for (let col = 0; col < board[row].length; col++) {
                 if (board[row][col] === player.move) consecutiveMovesCount++;
             }
-            if (consecutiveMovesCount === consecutiveMovesForWin)
+            if (consecutiveMovesCount === consecutiveMovesForWin) {
                 return `${player.name} wins!`;
+            }
         }
         consecutiveMovesCount = 0;
 
@@ -77,6 +100,7 @@ function gameController() {
     };
 
     return {
+        printBoard,
         getCurrentPlayer,
         switchCurrentPlayer,
         isValidMove,
@@ -86,6 +110,25 @@ function gameController() {
 }
 
 const game = gameController();
-while (!game.checkWin(GameBoard.getBoard(), game.getCurrentPlayer())) {
-    
+
+while (true) {
+    let currentPlayer = game.getCurrentPlayer();
+    let board = GameBoard.getBoard();
+
+    console.log(`It is ${currentPlayer.name}'s turn`);
+    let x = prompt("Enter row");
+    let y = prompt("Enter column");
+
+    if (game.isValidMove(x, y)) {
+        game.updateBoard(x, y);
+        game.switchCurrentPlayer();
+        game.printBoard(board);
+    } else {
+        console.log("Invalid move.");
+    }
+
+    if (game.checkWin(board, currentPlayer)) {
+        console.log(game.checkWin(board, currentPlayer));
+        break;
+    }
 }
